7486f5308ba07b79480af0358167e1c4d3e08dde
Josefreitas788 <josefreitas788@gmail.com>
2022-11-06 22:14:48 -0300

    parte da solução da questão

---
+++ codeforces/B._Kevin_and_Permutation.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+  int n, aux, tamanho;
+  vector<int> nums, aux_vet, aux_vet2;
+  cin >> n;
+
+  for(int i = 0; i < n; i++){
+    cin >> aux;
+    nums.push_back(aux);
+
+  }
+  for(int i = 0; i < n; i++){
+    for(int j = 1; j <= nums[i]; j++){
+      aux_vet.push_back(j);
+    }
+    tamanho = aux_vet.size();
+        if(tamanho %2 == 0){
+          for(int k = 1; k <= tamanho /2; k++){
+            aux_vet2.push_back(aux_vet[(tamanho/2)-k]);
+            aux_vet2.push_back(aux_vet[tamanho-k]);
+          
+          }
+        }
+        else{
+          aux_vet2.push_back(aux_vet[0]);
+          for(int k = 1; k <= tamanho/2; k++){
+                    
+            aux_vet2.push_back(aux_vet[(tamanho/2+1)-k]);
+            cout << aux_vet[(tamanho/2+1)-k] << " SLICE 1" << endl;
+            aux_vet2.push_back(aux_vet[tamanho-k]);
+            cout << aux_vet[tamanho-k] << " SLICE 2" << endl;
+          }
+
+        }
+    for(int j = 0; j < aux_vet2.size(); j++){
+      cout << aux_vet2[j] << " ";
+    }
+    cout << endl;
+  }
+}
+
+
