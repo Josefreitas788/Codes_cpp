1e5e9a464b9cb3e2925ce869f14384d4b1f96516
José Antônio <josefreitas788@gmail.com>
2024-11-20 00:24:29 -0300

    add-two-numbers WIP

---
+++ leetCode/add-two-numbers/add-two-numbers.go
@@ -0,0 +1,21 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	var sumValue int
+	decimal := 1
+	for {
+		if li
+		sumValue = sumValue + (l1.Val+l2.Val)*decimal
+		li, l2 := ne
+	}
+	return nil
+}
+
+func main() {
+
+}
