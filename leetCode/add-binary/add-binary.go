56dfc0df9c501ac4881e1f88d9198b49bd6b2a98
José Antônio <josefreitas788@gmail.com>
2024-11-19 23:01:06 -0300

    add-binary finished

--- leetCode/add-binary/add-binary.go
+++ leetCode/add-binary/add-binary.go
@@ -1,26 +1,30 @@
 package main
 
+import "fmt"
+
 func addBinary(a string, b string) string {
 	var rest byte
 	var result byte
 
+	rest = '0'
 	a, b = lengthBinaries(a, b)
-	var sum []byte
-	for i := len(a) - 1; i > len(a); i-- {
-		if i < len(b) {
-			result, rest = sumWithRest(a[i], b[i], rest)
+	var sum string
+	j := len(b) - 1
+	for i := len(a) - 1; i >= 0; i-- {
+		if j >= 0 {
+			result, rest = sumWithRest(a[i], b[j], rest)
+			j--
 		} else {
-			result = a[i]
+			result, rest = sumRules(a[i], rest)
 		}
-		sum = append(sum, result)
+		sum = string(result) + sum
 
 	}
+
 	if rest == '1' {
-		sum = append(sum, '1')
+		sum = string(rest) + sum
 	}
-	for i := 0; i < count; i++ {
-
-	}
+	return sum
 }
 
 func lengthBinaries(a, b string) (string, string) {
@@ -38,7 +42,7 @@ func sumWithRest(a, b, c byte) (result, rest byte) {
 	if result == '0' {
 		return '1', rest
 	}
-	return result, '1'
+	return '0', '1'
 }
 
 func sumRules(a, b byte) (result, rest byte) {
@@ -52,5 +56,21 @@ func sumRules(a, b byte) (result, rest byte) {
 	case a == '1' && b == '1':
 		return '0', '1'
 	}
+	return '0', '0'
+}
 
+func main() {
+
+	a := "110010"
+	b := "10111"
+
+	/*
+				 110010
+				  10111
+		right	1001001
+				1001101
+
+	*/
+	sum := addBinary(a, b)
+	fmt.Print(sum)
 }
